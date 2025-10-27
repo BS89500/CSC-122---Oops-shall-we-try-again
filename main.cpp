1617733253081443eb01677f070fea6a6d344da3
BS89500 <sb89500@mail.harpercollege.edu>
2025-10-26 23:46:50 -0500

    Update main.cpp ec

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-  oops(0, 100, "Please enter a value: ", "Invalid Input");
+  oops(0, 100, "Please enter a value (Enter 'default' to use a default value or exit to leave): ", "Invalid Input");
 
   // Your driver program goes here
 }
